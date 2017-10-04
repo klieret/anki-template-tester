d2a7c550d8317db735adfd56773b920f14f55b69
klieret <klieret@users.noreply.github.com>
2017-10-04 13:50:02 +0200

    refactor into TemplateTester class; separate interface and actual code

--- template-tester.py
+++ template-tester.py
@@ -60,101 +60,141 @@ def setup_logger(name="Logger"):
 
     return _logger
 
-logger = setup_logger("TemplateTest")
 
 
-parser = argparse.ArgumentParser(description="Test Anki templates, i.e. fill in field values and CSS to get a HTML file that can be displayed in the web browser.")
-parser.add_argument("template", help="The HTML template", type=str)
-parser.add_argument("fields", help="python file with dictionary containing field values", type=str)
-parser.add_argument("-s", "--style", help="Include CSS style sheet")
-parser.add_argument("-o", "--output", help="Output HTML file", type=str, default="")
-args = parser.parse_args()
 
-if not args.output:
-    args.output = os.path.splitext(args.template)[0] + "_out.html"
-
-html_out = "<html>"
-html_out += "<head>"
-html_out += '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
-html_out += "<title>{}</title>".format(args.template)
-
-if args.style:
-    if os.path.exists(args.style):
-
-        html_out += "<style>"
-        with open(args.style, "r") as css_file:
-            html_out += css_file.read()
-        html_out += "</style>"
-    else:
-        print("CSS file could not be found at {}".format(
-            os.path.abspath(args.style)))
-        sys.exit(1)
-
-html_out += "</head>"
-html_out += "<body>"
-
-if not os.path.exists(args.template):
-    logger.critical("HTML file/template could not be found at {}".format(
-            os.path.abspath(args.template)))
-    sys.exit(1)
-
-fields = None
-if os.path.exists(args.fields):
-    # todo: this is not nice
-    exec(open(args.fields).read())
-else:
-    logger.critical("fields file could not be found at {}".format(
-            os.path.abspath(args.fields)))
-    sys.exit(1)
-
-remove_following = False
-remove_close_field = None
-with open(args.template) as template_file:
-    for line in template_file:
-        #print(line)
-        # fixme: only works if there is only one conditional statement per line
-        match = re.search("\{\{#([a-zA-Z_ 0-9]*)\}\}", line)
-        if match:
-            fld = match.group(1)
-            #print(fld, "cond")
-            if fld in fields:
-                #print("in")
-                if not fields[fld].strip():
-                    #print("remove!")
-                    remove_following = True
-                    remove_close_field = fld
-            continue
-        match = re.search("\{\{\^([a-zA-Z_ 0-9]*)\}\}", line)
-        if match:
-            fld = match.group(1)
-            #print(fld, "neg cond")
-            if fld in fields:
-                #print("in")
-                if fields[fld].strip():
-                    remove_following = True
-                    remove_close_field = fld
-                    #print("remove!")
-            continue
-        match = re.search("\{\{\/([a-zA-Z_ 0-9]*)\}\}", line)
-        if match:
-            fld = match.group(1)
-            #print(fld, "cond end")
-            if fld == remove_close_field:
-                #print("in")
-                #print("stop remove!")
-                remove_following = False
-                remove_close_field = None
-            continue
-        if remove_following:
-            continue
-        # and try to replace the fields with the field values above
-        for field_name in fields.keys():
-            line = re.sub("\{\{" + field_name + "\}\}", fields[field_name], line)
-        html_out += line
-
-html_out += "</body>"
-html_out += "</html>"
+def get_cli_args():
+    parser = argparse.ArgumentParser(description="Test Anki templates, i.e. fill in field values and CSS to get a HTML file that can be displayed in the web browser.")
+    parser.add_argument("template", help="The HTML template", type=str)
+    parser.add_argument("fields", help="python file with dictionary containing field values", type=str)
+    parser.add_argument("-s", "--style", help="Include CSS style sheet")
+    parser.add_argument("-o", "--output", help="Output HTML file", type=str, default="")
+    return parser.parse_args()
 
 
-with open(args.output, "w") as output_file:
-    output_file.write(html_out)
+
+
+class TemplateTester(object):
+    def __init__(self, template: str, fields: dict, css: str):
+        self.html = ""
+        self.template = template
+        self.fields = fields
+        self.css = css
+
+    def render(self) -> str:
+        self.start_html_file()
+        self.main_loop()
+        self.end_html_file()
+        return self.html
+
+    def start_html_file(self):
+        self.html = "<html>"
+        self.html += "<head>"
+        self.html += '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
+        self.html += "<title>{}</title>".format(args.template)
+
+        if self.css:
+            self.html += "<style>"
+            self.html += self.css
+            self.html += "</style>"
+        
+        self.html += "</head>"
+        self.html += "<body>"
+
+    def end_html_file(self):
+        self.html += "</body>"
+        self.html += "</html>"
+
+    def main_loop(self):
+        remove_following = False
+        remove_close_field = None
+        lines = self.template.split("\n")
+        for line in lines:
+            #print(line)
+            # fixme: only works if there is only one conditional statement per line
+            match = re.search("\{\{#([a-zA-Z_ 0-9]*)\}\}", line)
+            if match:
+                fld = match.group(1)
+                #print(fld, "cond")
+                if fld in fields:
+                    #print("in")
+                    if not fields[fld].strip():
+                        #print("remove!")
+                        remove_following = True
+                        remove_close_field = fld
+                continue
+            match = re.search("\{\{\^([a-zA-Z_ 0-9]*)\}\}", line)
+            if match:
+                fld = match.group(1)
+                #print(fld, "neg cond")
+                if fld in fields:
+                    #print("in")
+                    if fields[fld].strip():
+                        remove_following = True
+                        remove_close_field = fld
+                        #print("remove!")
+                continue
+            match = re.search("\{\{\/([a-zA-Z_ 0-9]*)\}\}", line)
+            if match:
+                fld = match.group(1)
+                #print(fld, "cond end")
+                if fld == remove_close_field:
+                    #print("in")
+                    #print("stop remove!")
+                    remove_following = False
+                    remove_close_field = None
+                continue
+            if remove_following:
+                continue
+            # and try to replace the fields with the field values above
+            for field_name in fields.keys():
+                line = re.sub("\{\{" + field_name + "\}\}", fields[field_name], line)
+            self.html += line
+
+
+if __name__ == "__main__":
+    logger = setup_logger("TemplateTest")
+    args = get_cli_args()
+
+    if not args.output:
+        args.output = os.path.splitext(args.template)[0] + "_out.html"
+        logger.warning("Output file name was automatically "
+                       "set to {}.".format(args.output))
+
+    css = ""
+    if args.style:
+        try:
+            with open(args.style) as css_file:
+                css = css_file.read()
+        except:
+            logger.error("Could not load css file from {}. "
+                         "Leaving it empty.").format(args.style)
+
+    template = ""
+    try:
+        with open(args.template) as template_file:
+            template = template_file.read()
+    except:
+        _ = "Could not load template file from {}. " \
+            "Abort.".format(args.template)
+        logger.critical(_)
+        raise
+
+    fields = {}
+    if args.fields:
+        try:
+            with open(args.fields) as field_file:
+                # todo: this is not nice
+                exec(field_file.read())
+        except:
+            _ = "Could not load template file from {}. " \
+                "Abort.".format(args.template)
+            raise
+
+    tt = TemplateTester(template, fields, css)
+    html_out = tt.render()
+
+    with open(args.output, "w") as output_file:
+        output_file.write(html_out)
+
+    logger.info("Output written to {}".format(args.output))
