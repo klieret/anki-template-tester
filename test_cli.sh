e5270cb4a05cb5bc0e7e209a50bf2ec4b2df7569
klieret <klieret@users.noreply.github.com>
2017-10-06 22:24:04 +0200

    multiple fixes

--- test_cli.sh
+++ test_cli.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-templatetester="python3 templatetester.py"
+templatetester="python3 previewtemplate.py"
 
 exit_code=0
 for folder in test_files/*/; do
     echo "Testing folder ${folder}"
-    if [[ -f "${folder}/style.css" ]]; then
+    if [ -f "${folder}/style.css" ]; then
         echo "Found css file"
         style="--style ${folder}/style.css"
     else
         echo "Warning: Did not find css file"
         style=""
     fi
-    ${templatetester}  $style --output "${folder}/test.html" "${folder}/template.html" "${folder/}fields.txt" 
+    ${templatetester}  ${style} --output "${folder}/test.html" "${folder}/template.html" "${folder}/fields.txt" 
     if cmp -s "${folder}/test.html" "${folder}/output.html"; then
         echo "Output as expected"
     else
--- test_units.py
+++ test_units.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from templatetester import *
+from previewtemplate import *
 
 
 class TestParsing(unittest.TestCase):
